f917b6f019a5daf26a3888cb52a2a33132fdb2d7
Vesta-nna <anna.dominguez.anna@gmail.com>
2023-12-14 09:22:42 +0100

    working and responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import Section from './components/Section'
+import logo from './images/icon-star.svg'
+
+const faqs = [
+  {
+    title: 'What is Frontend Mentor, and how will it help me?',
+    description: `Frontend Mentor offers realistic coding challenges to help developers
+          improve their frontend coding skills with projects in HTML, CSS, and
+          JavaScript. It’s suitable for all levels and ideal for portfolio
+          building.`,
+  },
+  {
+    title: 'Is Frontend Mentor free?',
+    description: `Frontend Mentor offers realistic coding challenges to help developers
+          improve their frontend coding skills with projects in HTML, CSS, and
+          JavaScript. It’s suitable for all levels and ideal for portfolio
+          building.`,
+  },
+  {
+    title: 'Can I use Frontend Mentor projects in my portfolio?',
+    description: `Frontend Mentor offers realistic coding challenges to help developers
+          improve their frontend coding skills with projects in HTML, CSS, and
+          JavaScript. It’s suitable for all levels and ideal for portfolio
+          building.`,
+  },
+  {
+    title: `How can I get help if I'm stuck on a challenge?`,
+    description: `Frontend Mentor offers realistic coding challenges to help developers
+          improve their frontend coding skills with projects in HTML, CSS, and
+          JavaScript. It’s suitable for all levels and ideal for portfolio
+          building.`,
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <img src={logo} alt="star icon logo" />
+        <h1>FAQs</h1>
       </header>
+      <main>
+        {faqs.map(({ title, description }, index) => (
+          <Section
+            key={`faq-${index}`}
+            isLast={index < faqs.length - 1}
+            title={title}
+            description={description}
+          />
+        ))}
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Section.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import minus from '../images/icon-minus.svg'
+import plus from '../images/icon-plus.svg'
+
+import './Section.scss'
+
+const Section = ({ title, description, isLast }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <section>
+      <div className="title" onClick={() => setIsOpen(!isOpen)}>
+        <h2>{title}</h2>
+        <img src={isOpen ? minus : plus} alt="open menu" />
+      </div>
+      {isOpen && <p>{description}</p>}
+      {isLast && <div className="divider"></div>}
+    </section>
+  )
+}
+
+export default Section
